8c266b1771308a2f1dd76ca4baaad2e0ecf2fff0
HaiLTh <haianhh44@gmail.com>
2025-07-26 11:41:09 +0900

    update

--- src/main/java/ra/webmvc/repository/CategoryRepository.java
+++ src/main/java/ra/webmvc/repository/CategoryRepository.java
@@ -1,12 +1,4 @@
 package ra.webmvc.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ra.webmvc.entity.Category;
-
-import java.util.Optional;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Optional<Category> findByCateName(String cateName);
+public class CategoryRepository {
 }
